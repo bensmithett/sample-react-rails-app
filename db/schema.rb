8988a61fe8c6420dc1148f564973e1ab3407ab3a
Ben Smithett <ben@bensmithett.com>
2014-11-17 17:54:06 +1100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140315030553) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.string   "author"
     t.text     "text"
